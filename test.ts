0c125ae232149a08d45fab98dabe19d6e161758f
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-26 15:37:31 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,3 @@
 // tests go here; this will not be compiled when this package is used as a library
-let a=bit.hex2number("3451f");
-bit.ShowNumber(a);
+let a=bit.numberToBit(0x39fa)
+basic.showString(a);
